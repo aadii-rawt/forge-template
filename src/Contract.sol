6222825a27a2add7c5682f76b0bf9cc0f30d3ec2
aadii-rawt <rawatadii060@gmail.com>
2026-03-10 21:07:49 +0530

    init

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/"
 
-contract Contract { }
+contract Contract {
+    uint public number ;
+
+    constructor (_number){
+            number = _number ;
+    }
+
+    function incremennt() public {
+        number++;
+    }
+
+    function decremtn (type name) public {
+        number--;
+    }
+ }
